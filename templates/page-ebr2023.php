acda88fff305aa9a80faf8ca1b63e62bca21f2de
Tod9197 <dancein18@yahoo.co.jp>
2025-03-15 20:52:49 +0900

    ギャラーページの修正

---
+++ templates/page-ebr2023.php
@@ -0,0 +1,80 @@
+<?php  
+/*
+Template Name: EBR2023 ギャラリー
+*/
+get_header();
+?>
+
+<!-- メインビジュアル -->
+<section class="mainVisual -gallery">
+    <h1 class="mainVisual__title -gallery">gallery</h1>
+</section>
+
+<section class="gallery" id="gallery">
+  <div class="inner">
+    <h2 class="galleryTitle">GALLERY (EBR2023)</h2>
+    <p class="gallery__text">過去開催の写真 (EBR2023)</p>
+
+    <div class="galleryImg__list">
+      <?php  
+      $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+      $galleryArgs = array(
+        'post_type'      => 'attachment',
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'posts_per_page' => 20,
+        'paged'          => $paged,
+        'orderby'        => 'rand',
+        'tax_query'      => array(
+          array(
+            'taxonomy' => 'media_category',
+            'field'    => 'slug',
+            'terms'    => 'ebr2024', // 固定
+          ),
+        ),
+      );
+
+      $media_query = new WP_Query($galleryArgs);
+
+      if ($media_query->have_posts()):
+        while ($media_query->have_posts()): $media_query->the_post();
+          $img_url = wp_get_attachment_url(get_the_ID());
+      ?>
+          <div class="galleryImg__listItem">
+            <img src="<?php echo esc_url($img_url); ?>" alt="ギャラリー画像">
+          </div>
+      <?php
+        endwhile;
+        wp_reset_postdata();
+      else:
+      ?>
+    </div>
+    <p>ギャラリー写真はありません</p>
+    <?php endif; ?>
+  </div>
+
+  <!-- ページネーション -->
+  <div class="galleryArchive__pagination">
+    <?php  
+    echo paginate_links(array(
+      'total'     => $media_query->max_num_pages,
+      'current'   => $paged,
+      'prev_text' => __('←'),
+      'next_text' => __('→'),
+    ));
+    ?>
+  </div>
+
+   <!-- モーダル -->
+  <div class="modal" id="imageModal">
+    <div class="modal__wrap">
+      <span class="modal__close">&times;</span>
+      <img class="modal__content" id="modalImage">
+      <div id="caption"></div>
+    </div>
+  </div>
+
+
+</section>
+
+<?php get_footer(); ?>
--- templates/page-gallery.php
+++ templates/page-gallery.php
@@ -38,132 +38,80 @@ get_header();
 </div> -->
 <!-- 申し込みリンクここまで -->
 
-<!-- 2024年度画像 -->
-<div class="galleryArchive__wrap">
-  <div class="inner -galleryArchive">
-    <div class="galleryTab">
-      <div class="galleryTab__buttonWrap">
-        <button class="galleryTab__button -active" id="js-gallery-tab2024">2024年</button><button class="galleryTab__button" id="js-gallery-tab2023">2023年</button>
-    </div>
-  </div>
-  <section class="galleryArchive -show" id="js-gallery2024">
-    <h2 class="galleryArchive__title">2024年度EBR</h2>
-    <div class="galleryArchive__list">
-      <?php  
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-      $args = array(
-        'post_type' => 'gallery',
-        'posts_per_page' => 16,
-        'category_name' => 'ebr2024',
-        'paged' => $paged
-      );
-      $gallery_query = new WP_Query($args);
-
-      if ($gallery_query->have_posts()) :
-        while ($gallery_query->have_posts()) : $gallery_query->the_post();
-          if (has_post_thumbnail()) : ?>
-      
-            <div class="galleryArchive__listItem">
-              <?php the_post_thumbnail('full', array('class' => '')); ?>
-            </div>
-      
-          <?php  
-          endif;
-        endwhile; 
-      ?>
-    </div>
-    <p class="galleryImg__listItem__text">※クリックで拡大できます</p>
-    <!-- ページネーション -->
-    <div class="galleryArchive__pagination">
-      <?php  
-      echo paginate_links(array(
-        'total' => $gallery_query->max_num_pages,
-        'current' => $paged,
-        'prev_text' => __('←'),
-        'next_text' => __('→'),
-      ));
-      ?>
-    </div>
-
-    <?php
-      wp_reset_postdata();
-      else :
-    ?>
-      <p>ギャラリー画像はありません。</p>
-    <?php endif; ?>
-  
-  <!-- モーダル -->
-  <!-- <div class="modal" id="imageModal" >
-    <div class="modal__wrap">
-    <span class="modal__close">&times;</span>
-    <img class="modal__content" id="modalImage">
-    <div id="caption"></div>
-    </div>
-  </div> -->
-</section>
-<!-- 2024年度画像ここまで -->
-
 <!-- 2023年度画像 -->
-<section class="galleryArchive -off" id="js-gallery2023">
-    <h2 class="galleryArchive__title">2023年度EBR</h2>
-    <div class="galleryArchive__list">
+<section class="gallery" id="gallery">
+  <div class="inner">
+    <h2 class="galleryTitle" data-en="画像集">GALLERY</h2>
+    <p class="gallery__text">過去開催の写真</p>
+
+    <!-- カテゴリの選択 -->
+    <div class="gallery__category">
+      <a href="<?php echo esc_url(add_query_arg('category', 'ebr2023', home_url('/gallery'))); ?>">EBR2023</a> |
+      <a href="<?php echo esc_url(add_query_arg('category', 'ebr2024', home_url('/gallery'))); ?>">EBR2024</a>
+    </div>
+
+    <div class="galleryImg__list">
       <?php  
-      $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-      $args = array(
-        'post_type' => 'gallery',
-        'posts_per_page' => 20,
-        'category_name' => 'ebr2023',
-        'paged' => $paged
+      $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+      $selected_category = isset($_GET['category']) ? sanitize_text_field($_GET['category']) : 'ebr2023';
+
+      $galleryArgs = array(
+        'post_type'      => 'attachment',
+        'post_mime_type' => 'image',
+        'post_status'    => 'inherit',
+        'posts_per_page' => 10, 
+        'paged'          => $paged,
+        'orderby'        => 'rand',
+        'tax_query'      => array(
+          array(
+            'taxonomy' => 'media_category',
+            'field'    => 'slug',
+            'terms'    => $selected_category,
+          ),
+        ),
       );
-      $gallery_query = new WP_Query($args);
 
-      if ($gallery_query->have_posts()) :
-        while ($gallery_query->have_posts()) : $gallery_query->the_post();
-          if (has_post_thumbnail()) : ?>
-      
-            <div class="galleryArchive__listItem">
-              <?php the_post_thumbnail('full', array('class' => '')); ?>
-            </div>
-      
-          <?php  
-          endif;
-        endwhile; 
+      $media_query = new WP_Query($galleryArgs);
+
+      if ($media_query->have_posts()):
+        while ($media_query->have_posts()): $media_query->the_post();
+          $img_url = wp_get_attachment_url(get_the_ID());
+      ?>
+          <div class="galleryImg__listItem">
+            <img src="<?php echo esc_url($img_url); ?>" alt="ギャラリー画像">
+          </div>
+      <?php
+        endwhile;
+        wp_reset_postdata();
+      else:
       ?>
     </div>
-    <p class="galleryImg__listItem__text">※クリックで拡大できます</p>
-    <!-- ページネーション -->
-    <div class="galleryArchive__pagination">
-      <?php  
-      echo paginate_links(array(
-        'total' => $gallery_query->max_num_pages,
-        'current' => $paged,
-        'prev_text' => __('←'),
-        'next_text' => __('→'),
-      ));
-      ?>
-    </div>
-
-    <?php
-      wp_reset_postdata();
-      else :
-    ?>
-      <p>ギャラリー画像はありません。</p>
+    <p>ギャラリー写真はありません</p>
     <?php endif; ?>
   </div>
-</section>
 
-<!-- モーダル -->
-  <div class="modal" id="imageModal" >
+  <p class="galleryImg__listItem__text">※クリックで拡大できます</p>
+
+  <!-- ページネーション -->
+  <div class="galleryArchive__pagination">
+    <?php  
+    echo paginate_links(array(
+      'total'     => $media_query->max_num_pages,
+      'current'   => $paged,
+      'prev_text' => __('←'),
+      'next_text' => __('→'),
+    ));
+    ?>
+  </div>
+
+  <!-- モーダル -->
+  <div class="modal" id="imageModal">
     <div class="modal__wrap">
-    <span class="modal__close">&times;</span>
-    <img class="modal__content" id="modalImage">
-    <div id="caption"></div>
+      <span class="modal__close">&times;</span>
+      <img class="modal__content" id="modalImage">
+      <div id="caption"></div>
     </div>
   </div>
 
-</div>
-
-
-<!-- 2023年度画像ここまで -->
-
+</section>
 <?php get_footer(); ?>
