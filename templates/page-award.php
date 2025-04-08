da75ead1234c095bc4cd93d78f1a046b412bd0e4
Tod9197 <dancein18@yahoo.co.jp>
2025-04-08 20:54:45 +0900

    受賞ページを作成

---
+++ templates/page-award.php
@@ -0,0 +1,77 @@
+<?php  
+
+/*
+Template Name: 過去の成績
+*/
+get_header();
+
+?>
+
+<!-- メインビジュアル -->
+<section class="mainVisual -award">
+    <h1 class="mainVisual__title -award">Award</h1>
+</section>
+
+<!-- 申し込みリンク -->
+<!-- <div class="requestLink">
+  <div class="requestLink__flex">
+    <a class="requestLink__item -guest" href="https://docs.google.com/forms/d/e/1FAIpQLScz-y88pjXqvP2gWjCGlr7LqggBdRvFXqW9cdBiWuRk3Pxg8Q/viewform" id="js-request-guest" target="_blank" rel="noopener noreferrer">
+      <p class="requestLink__text">(ゲストの方)<br>　申し込む</p>
+    </a>
+    <a class="requestLink__item -student" href="https://docs.google.com/forms/d/e/1FAIpQLSdB7oQH7ayFtWY23RTtxR809xoHlZo_noQ2SdcgFE87GA0DIw/viewform" id="js-request-student" target="_blank" rel="noopener noreferrer">
+      <p class="requestLink__text -student">(門下生)<br>申し込む</p>
+    </a>
+  </div>
+  <a href="#" class="backToTop__button" id="backToTop">↑</a>
+</div>
+
+<div class="requestLink -mobile">
+  <div class="requestLink__flex -mobile">
+    <a class="requestLink__item -guest" href="https://docs.google.com/forms/d/e/1FAIpQLScz-y88pjXqvP2gWjCGlr7LqggBdRvFXqW9cdBiWuRk3Pxg8Q/viewform" id="js-request-guest" target="_blank" rel="noopener noreferrer" rel="noopener noreferrer">
+      <p class="requestLink__text">申し込む<br>(ゲストの方)</p>
+    </a>
+    <a class="requestLink__item -student" href="https://docs.google.com/forms/d/e/1FAIpQLSdB7oQH7ayFtWY23RTtxR809xoHlZo_noQ2SdcgFE87GA0DIw/viewform" id="js-request-student" target="_blank">
+      <p class="requestLink__text -student">申し込む<br>(門下生)</p>
+    </a>
+  </div>
+  <a href="#" class="backToTop__button -mobile" id="backToTop">↑</a>
+</div> -->
+<!-- 申し込みリンクここまで -->
+
+<!-- 画像集 -->
+<section class="award">
+  <div class="inner">
+    <div class="awardWrap">
+      <div class="award__outLine">
+        <div class="award__InLine">
+        <h2 class="awardTitle">団体戦</h2>
+        <table class="awardTable">
+          <tbody>
+            <tr>
+              <th class="awardTable__bold">開催年</th>
+              <th class="awardTable__bold">優勝</th>
+              <th class="awardTable__bold">準優勝</th>
+              <th class="awardTable__bold">開催場所</th>
+            </tr>
+            <tr>
+              <th>2023年</th>
+              <th>Team e-Go!</th>
+              <th>Team H.Blacks</th>
+              <th>東京</th>
+            </tr>
+            <tr>
+              <th>2024年</th>
+              <th>Team H.Blacks</th>
+              <th>Team e-Go!</th>
+              <th>東京</th>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  <a class="toTop__button" href="../">TOPへ戻る</a>
+</section>
+
+<?php get_footer(); ?>
