890979cc65c372fdb67c659fbc5936ecd8fdcf3a
Tod9197 <dancein18@yahoo.co.jp>
2025-06-29 19:43:44 +0900

    ポイント速報ページ追加

---
+++ templates/page-points-battle.php
@@ -0,0 +1,27 @@
+<?php  
+/*
+Template Name: 年間ポイント速報
+*/
+get_header();
+
+?>
+
+<section class="mainVisual -pointsBattle">
+    <h1 class="mainVisual__title -pointsBattle">Points</h1>
+</section>
+
+<section class="pointsBattle">
+  <div class="inner">
+    <h2 class="pointsBattle__title">Team H.Blacks VS Team e-Go!</h2>
+    <div class="pointsBattle__area">
+      <ul class="pointsBattle__chart">
+      <p>Team H.Blacks</p>
+        <div class="pointsBattle__chartItem -HB">
+        </div>
+        <p>Team e-Go!</p>
+        <li class="pointsBattle__chartItem -EG">
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
