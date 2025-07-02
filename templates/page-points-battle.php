4301421ce1e1d3bb906bf7c2c551ba5f4393a75a
Tod9197 <dancein18@yahoo.co.jp>
2025-07-02 20:23:47 +0900

    ポイントバーの実装

--- templates/page-points-battle.php
+++ templates/page-points-battle.php
@@ -13,16 +13,24 @@ get_header();
 <section class="pointsBattle">
   <div class="inner">
     <h2 class="pointsBattle__title">
-      <span class="ego">Team e-Go!</span> VS <span class="hb">Team H.Blacks</span>  </h2>
+      <span class="ego">Team e-Go!</span> VS <span class="hb">Team H.Blacks</span></h2>
     <div class="pointsBattle__area">
       <ul class="pointsBattle__chart">
         <li class="pointsBattle__chartItem">
           <span class="pointsBattle__chartText -ego">Team e-Go!</span>
-          <p class="pointsBattle__chartBar -ego"></p>
+          <p class="pointsBattle__chartBar -ego">
+          <span class="pointsBattle__chartPointWrapper">
+          <span class="pointsBattle__chartPoint">1324.0</span>
+          </span>
+          </p>
         </li>
         <li class="pointsBattle__chartItem">
           <span class="pointsBattle__chartText -hb">Team H.Blacks</span>
-          <p class="pointsBattle__chartBar -hb"></p>
+          <p class="pointsBattle__chartBar -hb">
+          <span class="pointsBattle__chartPointWrapper">
+          <span class="pointsBattle__chartPoint">1625.5</span>
+          </span>
+          </p>
         </li>
       </ul>
     </div>
