2e7a3e7be11d12fe5b3f6d642e36bb0f66566c26
Tod9197 <dancein18@yahoo.co.jp>
2025-07-29 19:41:58 +0900

    英文字修正

--- templates/interview.php
+++ templates/interview.php
@@ -6,9 +6,8 @@ get_header();
 ?>
 <!-- メインビジュアル -->
 <section class="mainVisual -interview">
-    <h1 class="mainVisual__title -intreview">Contest</h1>
+    <h1 class="mainVisual__title -intreview">contest</h1>
 </section>
-
 <section class="sponcers">
   <div class="inner">
     <div class="singleTopics__wrap">
@@ -16,9 +15,9 @@ get_header();
       <div class="timeTable__wrap">
         <div class="timeTable__outline">
           <div class="timeTable__inline">
-            <h2 class="sponsors__title">Interview</h2>
-            <p class="contest__subTitle">Comming Soon!</p>    
-            <p>詳細は後日、追って連絡いたします。</p>        
+            <h2 class="sponsors__title">interview</h2>
+            <p class="contest__subTitle">comming soon!</p>    
+            <p>詳細は追って連絡いたします。</p>        
           </div>
         </div>
       </div>
@@ -26,6 +25,5 @@ get_header();
 </div>
   </div>
 </section>
-
 <!-- フッター -->
 <?php get_footer(); ?>
