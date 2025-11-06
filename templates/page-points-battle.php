036aec1eedffaba5868675f7dc7c5b74bb0c2aea
Tod9197 <dancein18@yahoo.co.jp>
2025-11-06 19:22:41 +0900

    ポイント更新

--- templates/page-points-battle.php
+++ templates/page-points-battle.php
@@ -20,7 +20,7 @@ get_header();
           <span class="pointsBattle__chartText -ego">Team e-Go!</span>
           <p class="pointsBattle__chartBar -ego">
           <span class="pointsBattle__chartPointWrapper">
-          <span class="pointsBattle__chartPoint">1807.0</span>
+          <span class="pointsBattle__chartPoint">1811.0</span>
           </span>
           </p>
         </li>
@@ -28,7 +28,7 @@ get_header();
           <span class="pointsBattle__chartText -hb">Team H.Blacks</span>
           <p class="pointsBattle__chartBar -hb">
           <span class="pointsBattle__chartPointWrapper">
-          <span class="pointsBattle__chartPoint">1949.0</span>
+          <span class="pointsBattle__chartPoint">1951.0</span>
           </span>
           </p>
         </li>
