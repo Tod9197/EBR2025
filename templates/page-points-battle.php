514a06097dd819b8fd93e37fe2e73357abd66a33
Tod9197 <dancein18@yahoo.co.jp>
2025-11-29 09:30:13 +0900

    ポイント更新

--- templates/page-points-battle.php
+++ templates/page-points-battle.php
@@ -20,7 +20,7 @@ get_header();
           <span class="pointsBattle__chartText -ego">Team e-Go!</span>
           <p class="pointsBattle__chartBar -ego">
           <span class="pointsBattle__chartPointWrapper">
-          <span class="pointsBattle__chartPoint">1999.0</span>
+          <span class="pointsBattle__chartPoint">2018.0</span>
           </span>
           </p>
         </li>
@@ -28,13 +28,13 @@ get_header();
           <span class="pointsBattle__chartText -hb">Team H.Blacks</span>
           <p class="pointsBattle__chartBar -hb">
           <span class="pointsBattle__chartPointWrapper">
-          <span class="pointsBattle__chartPoint">2091.0</span>
+          <span class="pointsBattle__chartPoint">2051.0</span>
           </span>
           </p>
         </li>
       </ul>
     </div>
-    <p class="pointsBattle__anotation">※2025年11月28日時点</p>
+    <p class="pointsBattle__anotation">※2025年11月29日時点</p>
     <a class="toPointLink" href="<?php echo esc_url(home_url('/points')); ?>">ポイントの換算方法はこちら</a>
     <a class="toPointLink -toTop" href="../">TOPへ戻る</a>
   </div>
